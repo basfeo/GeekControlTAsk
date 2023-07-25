0e348eed561b41e06c2bb0626753c2ce8c640eff
Vasily <48855460+basfeo@users.noreply.github.com>
2023-07-25 16:07:23 +0300

    Added comm to code

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
-﻿
+﻿// Array from task.
 string[] array1 = {"Hello", "2", "world", ":-)"};
 string[] array2 = {"1234", "1567", "-2", "computer science"};
 string[] array3 = {"Russia", "Denmark", "Kazan"};
 
 int LengthArr(string[] arr)
 {
-    
+    // This function is for calculating the dimensionality of an array
     int index = 0;
     
     for (int i = 0; i < arr.Length; ++i)
@@ -20,7 +20,7 @@ int LengthArr(string[] arr)
 
 string[] CrtStrArr(string[] arr)
 {
-    
+    // This function is for obtaining a new array after processing
     string[] NewStrArr = new string[LengthArr(arr)];
     int j = LengthArr(arr)-1;
     
@@ -37,7 +37,7 @@ string[] CrtStrArr(string[] arr)
 
 void PrintArray(string[] arrayForPrint)
 {
-    
+    // This function is for printing array
     Console.WriteLine("[" + string.Join(", ", arrayForPrint) + "]");
 }
 
